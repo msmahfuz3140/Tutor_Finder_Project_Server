47b1836f897920396f554ebe6735a2989c71109f
msmahfuz3140 <mdmahfuzulhaque3140@gmail.com>
2026-05-20 15:43:17 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const cors = require("cors")
+
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const PORT = process.env.PORT || 4000;
+const uri = process.env.MONGODB_URI;
+app.use(cors());
+app.use(express.json());
+if (!uri) {
+    throw new Error("❌ MONGODB_URI missing in .env file");
+}
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("✅ MongoDB Connected Successfully");
+
+        const db = client.db("tutors-finder");
+        const tutorsCollection = db.collection("tutors");
+
+        app.get("/", (req, res) => {
+            res.send("Server is Running now");
+        });
+
+        app.get("/tutors", async (req, res) => {
+            try {
+                const tutors = await tutorsCollection.find({}).toArray();
+                res.status(200).json(tutors);
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ error: "Failed to fetch tutors" });
+            }
+        });
+
+        app.post("/tutors", async (req, res) => {
+            try {
+                const tutorsData = req.body;
+
+                const result = await tutorsCollection.insertOne(tutorsData);
+
+                res.status(201).json(result);
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ error: "Failed to insert tutor" });
+            }
+        });
+
+        app.listen(PORT, () => {
+            console.log(`🚀 Server running on PORT ${PORT}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+run();
