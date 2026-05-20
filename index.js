df63c6399b21a736d5489d9b764b1f27b0cee6ec
msmahfuz3140 <mdmahfuzulhaque3140@gmail.com>
2026-05-20 21:22:03 +0600

    added uri

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const PORT = process.env.PORT || 4000;
 const uri = process.env.MONGODB_URI;
-app.use(cors());
+app.use(cors({
+    origin: "https://tutor-finder-project.vercel.app",
+    credentials: true
+  }));
 app.use(express.json());
 if (!uri) {
     throw new Error("❌ MONGODB_URI missing in .env file");
